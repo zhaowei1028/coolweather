483294b96a2074849195d83eeddc4d0dd368e204
ZhaoWei <zhaowei_dlzw@qq.com>
2017-10-06 19:14:39 +0800

    完成天气预报功能

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -112,5 +114,26 @@ public class Utility {
         }
 
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     * @param response
+     * @return
+     */
+    public static Weather handleWeatherResponse(String response){
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather5");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (Exception ex){
+
+            ex.printStackTrace();
+        }
+
+        return null;
     }
 }
