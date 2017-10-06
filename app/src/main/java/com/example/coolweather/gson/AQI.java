a72aa8292609696e7c7b3df78d4a4d33241f74fd
ZhaoWei <zhaowei_dlzw@qq.com>
2017-10-06 10:38:12 +0800

    添加空气质量实况和预报实体类

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/10/6.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+        public String pm25;
+    }
+}
